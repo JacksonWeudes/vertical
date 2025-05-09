227fc31e89ff10d174c31636923ffa0f9fbc5d4b
JacksonWeudes <weudes2024jackson@gmail.com>
2025-05-09 15:58:30 -0300

    página mobile principal finalizada

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 const menu = document.querySelector(".side-menu")
-const overlay = document.querySelector('.overlay')
+const overlay = document.querySelector("#overlay")
+const loginScreen = document.getElementById("login-screen")
+const exitbtn = document.getElementById("exit-btn")
 
 function menuToggle() {
-    if (menu.style.left === '-15rem') {
+    if (menu.style.left === "-15rem") {
         menu.style.left = '0'
-        overlay.style.visibility = 'visible'
     }else {
         menu.style.left = '-15rem'
-        overlay.style.visibility = 'hidden'
+    }
+}
+
+function openLogin() {
+    if (loginScreen.style.display === "none") {
+        loginScreen.style.display = "block"
+        overlay.style.display = "block"
+        document.body.classList.add("no-scroll")
+    }else {
+        loginScreen.style.display = "none"
+        overlay.style.display = "none"
+        document.body.classList.remove("no-scroll")
+    }
+}
+
+function closeLogin() {
+        if (loginScreen.style.display === "block") {
+        loginScreen.style.display = "none"
+        overlay.style.display = "none"
+        document.body.classList.remove("no-scroll")
     }
 }
